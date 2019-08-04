566b03d34b4ae9e1a2137dfe021a57a0838fc138
Drank-pour <950doenut.buff@gmail.com>
2019-08-04 13:28:21 -0400

    Add files via upload

--- register.php
+++ register.php
@@ -1,5 +1,26 @@
 <?php
     include 'post.php';
+    require_once 'Mail.php';
+    function emailUser($address,$user){
+      //From
+      $from = '950doenut.buff@gmail.com';
+      $to = "$address";
+      $subject = "Verification";
+      $msg = "This a test email to verify your registration. You username is $user";
+
+      $headers = array('From' => $from,'To'=> $to,"Subject" => $subject);
+      $smtp = Mail::factory('smtp',array('host'=> 'ssl://smtp.gmail.com',
+      'port'=>'465', 'auth'=> true,'username' => '950doenut.buff@gmail.com','password' => 'Xxtriggered_911xX'));
+      
+      //Send email
+      $mail = $smtp -> send($to,$headers,$msg);
+      if(PEAR::isError($mail)){
+        echo"<h1> . $mail->getMessage() . </h1>";
+      }
+      else{
+        echo"<h1 align='Center'>An email will be sent to you for verification</h1>";
+      }
+    }
     if(isset($_POST['submitButton'])){
         $userName =  strtolower($_POST['username']);
         $password = strtolower($_POST['password']);
@@ -20,7 +41,9 @@
           }
           else{
             if($request = $connection->query($insert)){
-              echo"New Record created";
+              emailUser($email,$userName);
+              echo"<p align = 'Center'>New Record created</p><br>";
+              
             }
           }
 
