804c2e0b21428d3b34e448df19143cbbc36dd1e0
Drank-pour <950doenut.buff@gmail.com>
2019-08-07 20:01:59 -0400

    Update main.php

--- main.php
+++ main.php
@@ -1,26 +1,62 @@
 <?php
   require "login.php";
-  if(!isset($_SESSION['username'])){
-      header('HTTP/1.0 403 Forbidden', TRUE, 404);
-  }
-  else{
-    echo"Username is ",(string)$_SESSION['username'];
-    echo"<!DOCTYPE html>
+?>
+<!DOCTYPE html>
     <html>
       <head>
         <meta charset='UTF-8'>
         <title>Main</title>
         <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm' crossorigin='anonymous'>
-    
+        <link rel ='stylesheet' href='register.css'>
       </head>
       <body>
-        <p>You are in</p><br><button><a href='endSession.php'>Logout</a></button>
+        <?php
+          if(!isset($_SESSION['username'])){
+            echo"<h1 align = 'Center'>You have not logged in.</h1>";
+          }
+          else{
+            $username = $_SESSION['username'];
+            echo"
+            <nav class='navbar navbar-expand-lg navbar-light bg-light'>
+            <a class='navbar-brand' href='#'>$username</a>
+            <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
+              <span class='navbar-toggler-icon'></span>
+            </button>
+          
+            <div class='collapse navbar-collapse' id='navbarSupportedContent'>
+              <ul class='navbar-nav mr-auto'>
+                <li class='nav-item active'>
+                  <a class='nav-link' href='endSession.php'>Logout <span class='sr-only'>(current)</span></a>
+                </li>
+                <li class='nav-item'>
+                  <a class='nav-link' href='#'>Link</a>
+                </li>
+                <li class='nav-item dropdown'>
+                  <a class='nav-link dropdown-toggle' href='#' id='navbarDropdown' role='button' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+                    Dropdown
+                  </a>
+                  <div class='dropdown-menu' aria-labelledby='navbarDropdown'>
+                    <a class='dropdown-item' href='#'>Action</a>
+                    <a class='dropdown-item' href='#'>Another action</a>
+                    <div class='dropdown-divider'></div>
+                    <a class='dropdown-item' href='#'>Something else here</a>
+                  </div>
+                </li>
+                <li class='nav-item'>
+                  <a class='nav-link disabled' href='#'>Disabled</a>
+                </li>
+              </ul>
+              <form class='form-inline my-2 my-lg-0'>
+                <input class='form-control mr-sm-2' type='search' placeholder='Search' aria-label='Search'>
+                <button class='btn btn-outline-success my-2 my-sm-0' type='submit'>Search</button>
+              </form>
+            </div>
+          </nav>";
+          }
+        ?>
         <!-- JAVASCRIPT_CDN -->
         <script src='https://code.jquery.com/jquery-3.2.1.slim.min.js' integrity='sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN' crossorigin='anonymous'></script>
         <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js' integrity='sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q' crossorigin='anonymous'></script>
         <script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js' integrity='sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl' crossorigin='anonymous'></script>
       </body>
-    </html>";
-
-  }
-?>
+</html>
