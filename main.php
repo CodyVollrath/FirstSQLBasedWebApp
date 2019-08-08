41ce00d8c2030122f902be6a7e23f9d35be15bf4
Drank-pour <950doenut.buff@gmail.com>
2019-08-07 23:44:58 -0400

    Add files via upload

--- main.php
+++ main.php
@@ -11,14 +11,23 @@
       </head>
       <body>
         <?php
-          if(!isset($_SESSION['username'])){
+          if(!isset($_SESSION['loginUsername'])){
             echo"<h1 align = 'Center'>You have not logged in.</h1>";
+            echo"<h2 align = 'Center'>Check your username or password";
           }
           else{
-            $username = $_SESSION['username'];
+            $username = $_SESSION['loginUsername'];
             echo"
             <nav class='navbar navbar-expand-lg navbar-light bg-light'>
-            <a class='navbar-brand' href='#'>$username</a>
+            <div class='navbar-brand dropdown'>
+              <a class = 'dropdown-toggle' id='navbarDropdown' role='button' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+                $username
+              </a>
+              <div class='dropdown-menu' aria-labelledby='navbarDropdown'>
+                  <a class='dropdown-item' href='endSession.php'>Logout</a>
+                  <a class='dropdown-item' href='#'>Account</a>
+              </div>
+            </div>
             <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
               <span class='navbar-toggler-icon'></span>
             </button>
@@ -26,37 +35,22 @@
             <div class='collapse navbar-collapse' id='navbarSupportedContent'>
               <ul class='navbar-nav mr-auto'>
                 <li class='nav-item active'>
-                  <a class='nav-link' href='endSession.php'>Logout <span class='sr-only'>(current)</span></a>
-                </li>
-                <li class='nav-item'>
-                  <a class='nav-link' href='#'>Link</a>
-                </li>
-                <li class='nav-item dropdown'>
-                  <a class='nav-link dropdown-toggle' href='#' id='navbarDropdown' role='button' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
-                    Dropdown
-                  </a>
-                  <div class='dropdown-menu' aria-labelledby='navbarDropdown'>
-                    <a class='dropdown-item' href='#'>Action</a>
-                    <a class='dropdown-item' href='#'>Another action</a>
-                    <div class='dropdown-divider'></div>
-                    <a class='dropdown-item' href='#'>Something else here</a>
-                  </div>
-                </li>
-                <li class='nav-item'>
-                  <a class='nav-link disabled' href='#'>Disabled</a>
+                  <a class='nav-link' href='#'>Coming Soon <span class='sr-only'>(current)</span></a>
                 </li>
               </ul>
-              <form class='form-inline my-2 my-lg-0'>
-                <input class='form-control mr-sm-2' type='search' placeholder='Search' aria-label='Search'>
-                <button class='btn btn-outline-success my-2 my-sm-0' type='submit'>Search</button>
+              <form class='mx-2 my-auto mx-auto search-bar d-inline w-50'>
+                <div class = 'input-group'>
+                    <input class=' form-control mr-sm-2' type='text' placeholder='Search Contacts' aria-label='Search'>
+                    <span class='input-group-append'>
+                    <button class='btn btn-outline-success' type='submit'>GO</button>
+                    </span>
+                </div>
               </form>
             </div>
-          </nav>";
+          </nav>
+          
+          ";
           }
         ?>
-        <!-- JAVASCRIPT_CDN -->
-        <script src='https://code.jquery.com/jquery-3.2.1.slim.min.js' integrity='sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN' crossorigin='anonymous'></script>
-        <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js' integrity='sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q' crossorigin='anonymous'></script>
-        <script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js' integrity='sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl' crossorigin='anonymous'></script>
       </body>
 </html>
