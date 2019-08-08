9f8cdb553237d4dee74aa691ee115ccf55c16905
Drank-pour <950doenut.buff@gmail.com>
2019-08-08 16:47:53 -0400

    Add files via upload

---
+++ login.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset='UTF-8'>
+        <title>Main</title>
+        <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm' crossorigin='anonymous'>
+        <link rel ='stylesheet' href='register.css'>
+      </head>
+      <body>
+<?php
+    include "post.php";
+    $connection = openCon();
+    session_start();
+    //Check if submit button was pressed
+    if(isset($_POST['submitButton'])){
+        //Declare variables and convert to lowercase except for password
+        $userName = strtolower($_POST['username']);
+        $password = $_POST['password'];
+
+        //Prepare statement
+        $userName = $connection->real_escape_string($userName);
+        $password = $connection->real_escape_string($password);
+
+        $query = "SELECT username,`password` FROM users WHERE username = '$userName'";
+        $checkCreds = $connection->query($query);
+        if($checkCreds){
+            if(mysqli_num_rows($checkCreds)== 1){
+                $result = $checkCreds->fetch_object();
+                $dbPassword = $result->password;
+                $pwdCheck = password_verify($password,$dbPassword);
+                if($pwdCheck){
+                    $_SESSION['loginUsername'] = $userName;
+                    $_SESSION['loginPassword'] = $password;
+                    header("Location: main.php");
+                }
+                else{
+                    echo"Check Password";
+                    $goBack = htmlspecialchars($_SERVER['HTTP_REFERER']);
+                    echo"<br><button><a href='$goBack'>Go Back</a></button>";
+                }
+            }
+            else{
+                echo"Check username and password";
+                $goBack = htmlspecialchars($_SERVER['HTTP_REFERER']);
+                echo"<br><button><a href='$goBack'>Go Back</a></button>";
+            }
+        }
+        else{
+            echo"ERROR";
+        }
+    }
+    CloseCon($connection);
+?>
+
+        <!-- JAVASCRIPT_CDN -->
+        <script src='https://code.jquery.com/jquery-3.2.1.slim.min.js' integrity='sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN' crossorigin='anonymous'></script>
+        <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js' integrity='sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q' crossorigin='anonymous'></script>
+        <script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js' integrity='sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl' crossorigin='anonymous'></script>
+      </body>
+</html>
---
+++ main.php
@@ -0,0 +1,78 @@
+<?php
+  require "login.php";
+?>
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <meta charset='UTF-8'>
+        <title>Main</title>
+        <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm' crossorigin='anonymous'>
+        <link rel='stylesheet' href='register.css'>
+    </head>
+
+    <body>
+        <?php
+          if(!isset($_SESSION['loginUsername'])){
+            echo"<h1 align = 'Center'>You have not logged in.</h1>";
+            echo"<h2 align = 'Center'>Check your username or password";
+          }
+          else{
+            $username = $_SESSION['loginUsername'];
+            echo"
+            <nav class='navbar navbar-expand-lg navbar-light bg-light'>
+            <div class='navbar-brand dropdown'>
+              <a class = 'dropdown-toggle' id='navbarDropdown' role='button' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+                $username
+              </a>
+              <div class='dropdown-menu dropdown-menu-left' aria-labelledby='navbarDropdown'>
+                  <a class='dropdown-item' href='endSession.php'>Logout</a>
+                  <a class='dropdown-item' href='#'>Account</a>
+              </div>
+            </div>
+            <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
+              <span class='navbar-toggler-icon'></span>
+            </button>
+          
+            <div class='collapse navbar-collapse' id='navbarSupportedContent'>
+              <ul class='navbar-nav mr-auto'>
+                <li class='nav-item active'>
+                  <a class='nav-link' href='#'>Coming Soon <span class='sr-only'>(current)</span></a>
+                </li>
+              </ul>
+              <form class='mx-2 my-auto mx-auto search-bar d-inline w-50'>
+                <div class = 'input-group'>
+                    <input class=' form-control mr-sm-2' type='text' placeholder='Search Contacts' aria-label='Search'>
+                    <span class='input-group-append'>
+                    <button class='btn btn-outline-success' type='submit'>GO</button>
+                    </span>
+                </div>
+              </form>
+            </div>
+          </nav>
+          <div class = 'contactContainer'>
+            <table>
+              <tr>
+                <th>Contact</th>
+              </tr>
+              <tr>
+                <td>Data</td>
+              </tr>
+            </table>
+          </div>
+          <div class = 'messageContainer'>
+              <table>
+                  <tr>
+                    <th>Message</th>
+                  </tr>
+                  <tr>
+                    <td>Data</td>
+                  </tr>
+                </table>
+          </div>
+          ";
+          }
+        ?>
+    </body>
+
+    </html>
