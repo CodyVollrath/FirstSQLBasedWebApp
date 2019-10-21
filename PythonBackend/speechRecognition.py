a8585877e8dbc31c7184bf6ebc9dd68e298078f5
CodyVollrath <950doenut.buff@gmail.com>
2019-10-21 13:17:11 -0400

    git commit

--- PythonBackend/speechRecognition.py
+++ PythonBackend/speechRecognition.py
@@ -6,11 +6,36 @@ Deals with leveraging the commands allowed to be used with speech recognition
 @Version 1.0
 '''
 import speech_recognition as speech
-
+import 
 class SpeechRecognition:
-    
-    def __init__(self,assitantName):
-        self.name = assitantName
-    
 
+    """
+    Initlizes the audio recognizer and allows for the input of an audio file
+    Uses the Houndify API to plug in to the wav file
+
+    PreCondition audio != null
+    postcondiiton none
+
+    """
+    def __init__(self):
+        self.CLIENT_ID_HOUNDIFY = "lrMJd04qNu4Tzuph_FSKZA=="
+        self.CLIENT_KEY_HOUNDIFY = "PwMu-ETv78Y4VM3Jo2IsbTJRlDJU_krlOoBqEfeCSzOH8bGOk84-u3_j44kPD4Q2DR9AD9YpZiB4OSXKZONeTQ=="
+        self.voiceParser = speech.Recognizer()
+        
+    def recordAudio(self):
+        with speech.Microphone() as source:
+            audio = self.voiceParser.listen(source)
+        return audio
+    
+    def displayTranscribedText(self,audio):
+        self.audioIntake = self.voiceParser.recognize_houndify(audio,self.CLIENT_ID_HOUNDIFY,self.CLIENT_KEY_HOUNDIFY)
+        print(self.audioIntake)
+
+def test():
+    testRecog = SpeechRecognition()
+    source = testRecog.recordAudio()
+    testRecog.displayTranscribedText(source)
+
+test()
+    
     
