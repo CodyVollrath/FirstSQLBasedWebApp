120b9efa08463ea9dc1758a7f40595e43b7b22d6
Cody Vollrath <950doenut.buff@gmail.com>
2019-12-02 13:59:48 -0500

    Delete emoProgramming.py

--- images/emoProgramming.py
+++
@@ -1,7 +0,0 @@
-#How much our heart is broken! :( 
-#As you can see this is working okay in the command line, but what if we want to write a program in a textdocument #and run it? Well I am going to write here
-
-#Input text program
-
-text = input("Enter text>>")
-print(text)
